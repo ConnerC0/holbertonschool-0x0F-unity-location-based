f836a2da7cf0ac3cccd2a6739aab9698ac7aefca
ConnerC0 <4908@holbertonschool.com>
2023-10-03 13:21:40 -0500

    Task 1 and 2

--- Location Based AR/Assets/Scripts/LocationDisplay.cs
+++ Location Based AR/Assets/Scripts/LocationDisplay.cs
@@ -8,6 +8,8 @@ public class LocationDisplay : MonoBehaviour
     public TextMeshProUGUI latitudeText;
     public TextMeshProUGUI longitudeText;
     public TextMeshProUGUI altitudeText;
+    public TextMeshProUGUI distanceText;
+    private LocationInfo storedLocation;
 
     private IEnumerator Start()
     {
@@ -64,5 +66,47 @@ public class LocationDisplay : MonoBehaviour
     private void OnDestroy()
     {
         Input.location.Stop();
+    }
+    
+    public void StoreCurrentLocation()
+    {
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            storedLocation = Input.location.lastData;
+            Debug.Log($"Stored location: Latitude = {storedLocation.latitude}, Longitude = {storedLocation.longitude}, Altitude = {storedLocation.altitude}");
+        }
+        else
+        {
+            Debug.LogError("Location services are not running. Cannot store the current location.");
+        }
+    }
+    
+    public void CalculateDistance()
+    {
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            LocationInfo currentLocation = Input.location.lastData;
+            float distance = CalculateDistanceBetweenCoordinates(storedLocation, currentLocation);
+            distanceText.text = $"Distance: {distance} meters";
+        }
+        else
+        {
+            Debug.LogError("Location services are not running. Cannot calculate the distance.");
+        }
+    }
+
+    private float CalculateDistanceBetweenCoordinates(LocationInfo location1, LocationInfo location2)
+    {
+        float earthRadius = 6371e3f; // Earth's radius in meters
+        float lat1 = Mathf.Deg2Rad * location1.latitude;
+        float lat2 = Mathf.Deg2Rad * location2.latitude;
+        float deltaLat = Mathf.Deg2Rad * (location2.latitude - location1.latitude);
+        float deltaLon = Mathf.Deg2Rad * (location2.longitude - location1.longitude);
+
+        float a = Mathf.Sin(deltaLat / 2) * Mathf.Sin(deltaLat / 2) +
+                  Mathf.Cos(lat1) * Mathf.Cos(lat2) * Mathf.Sin(deltaLon / 2) * Mathf.Sin(deltaLon / 2);
+        float c = 2 * Mathf.Atan2(Mathf.Sqrt(a), Mathf.Sqrt(1 - a));
+
+        return earthRadius * c;
     }
 }
