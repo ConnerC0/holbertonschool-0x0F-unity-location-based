b2a364461cbfd8c64095a490e0c55c80500abcbe
ConnerC0 <4908@holbertonschool.com>
2023-10-03 09:35:16 -0500

    location permission request

--- Location Based AR/Assets/Scripts/LocationDisplay.cs
+++ Location Based AR/Assets/Scripts/LocationDisplay.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using UnityEngine.Android;
 
 public class LocationDisplay : MonoBehaviour
 {
@@ -12,11 +13,25 @@ public class LocationDisplay : MonoBehaviour
     {
         if (!Input.location.isEnabledByUser)
         {
-            Debug.LogError("Location services are not enabled.");
+            Debug.LogError("Location services are not enabled. Status: " + Input.location.status);
             yield break;
         }
 
-        Input.location.Start();
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+            {
+                Permission.RequestUserPermission(Permission.FineLocation);
+            }
+
+            // Wait for the user to authorize the permission
+            while (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+            {
+                yield return new WaitForSeconds(1);
+            }
+        }
+
+        Input.location.Start(10, 10);
 
         while (Input.location.status == LocationServiceStatus.Initializing)
         {
